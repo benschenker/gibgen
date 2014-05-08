81e20f9ec9e48e9e2b292696c0795fb02a700ae3
benschenker <binsch@gmail.com>
2014-05-08 19:21:15 -0400

    handled ACRONYMS, read from stdin or file
    also removed print statements

--- gibgen.py
+++ gibgen.py
@@ -1,34 +1,29 @@
 import fileinput, string,math,random
 
-
-
 def gibify(word):
     swappable_char_indices=[]
     for i,char in enumerate(word):
         if i!=0 and char in string.ascii_letters:
             swappable_char_indices.append(i)
     
-    if len(swappable_char_indices)>2:
+    if len(swappable_char_indices)>2 and not word.isupper():
         swappable_char_indices.pop()
         return swap(word,swappable_char_indices)
     else:
         return word
 
-
 def swap(word,indices):
-    print word,indices
     chars = list(word)
     new_indices = indices[:]
     random.shuffle(new_indices)
     chars_copy = chars[:]
     for i,j in zip(indices,new_indices):
-        print word, i,j
         chars[j]=chars_copy[i]
     return "".join(chars)
 
-line = "We're"
-output=""
-tokens = line.split(" ")
-for token in tokens:
-    output+=gibify(token)+" "
-print output[:-1]
+for line in fileinput.input():
+    output=""
+    tokens = line.split(" ")
+    for token in tokens:
+        output+=gibify(token)+" "
+    print output[:-1]
